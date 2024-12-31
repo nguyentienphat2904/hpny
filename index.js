c219df5ddb7e89cc8dff8ee31c2883ede7d0e95e
nguyentienphat2904 <phat.nguyen221252729@hcmut.edu.vn>
2024-12-31 22:50:45 +0700

    2025

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
+let h1 = document.querySelector('h1');
 let h2 = document.querySelector('h2');
 let container = document.querySelector('.container');
-h2.onclick = function() {
-    container.classList.toggle('newyear');
-    this.classList.toggle('active');
 
+function triggerNewYearEffect() {
+    container.classList.toggle('newyear');
+    h2.classList.toggle('active');
     firework();
 }
 
-function firework(){
+
+function startCountDown(seconds) {
+    h1.textContent = `${seconds}`;
+    let interval = setInterval(() => {
+        seconds--;
+        if (seconds > 0) {
+            h1.textContent = `${seconds}`;
+        } else {
+            clearInterval(interval);
+            h1.textContent = "";
+            triggerNewYearEffect();
+        }
+    }, 1000);
+}
+
+function checkNewYear() {
+    let now = new Date();
+    if (now.getHours() == 23 && now.getMinutes() == 59 && now.getSeconds() == 50
+        && now.getDate() == 31 && now.getMonth() == 11
+    ) {
+        startCountDown(10);
+    }
+}
+
+// h2.onclick = function () {
+//     triggerNewYearEffect();
+// }
+
+setInterval(checkNewYear, 1000);
+
+function firework() {
     var c = document.getElementById("Canvas");
     var ctx = c.getContext("2d");
 
     var cwidth, cheight;
     var shells = [];
-    var pass= [];
+    var pass = [];
 
     var colors = ['#FF5252', '#FF4081', '#E040FB', '#7C4DFF', '#536DFE', '#448AFF', '#40C4FF', '#18FFFF', '#64FFDA', '#69F0AE', '#B2FF59', '#EEFF41', '#FFFF00', '#FFD740', '#FFAB40', '#FF6E40'];
 
-    window.onresize = function() { reset(); }
+    window.onresize = function () { reset(); }
     reset();
     function reset() {
 
@@ -28,38 +59,38 @@ function firework(){
     }
 
     function newShell() {
-    
+
         var left = (Math.random() > 0.5);
         var shell = {};
-        shell.x = (1*left);
+        shell.x = (1 * left);
         shell.y = 1;
         shell.xoff = (0.01 + Math.random() * 0.007) * (left ? 1 : -1);
         shell.yoff = 0.01 + Math.random() * 0.007;
         shell.size = Math.random() * 6 + 3;
         shell.color = colors[Math.floor(Math.random() * colors.length)];
-    
+
         shells.push(shell);
     }
 
     function newPass(shell) {
-    
+
         var pasCount = Math.ceil(Math.pow(shell.size, 2) * Math.PI);
-    
+
         for (i = 0; i < pasCount; i++) {
-    
+
             var pas = {};
             pas.x = shell.x * cwidth;
             pas.y = shell.y * cheight;
-    
+
             var a = Math.random() * 4;
             var s = Math.random() * 10;
-    
-                pas.xoff = s *  Math.sin((5 - a) * (Math.PI / 2));
-            pas.yoff = s *  Math.sin(a * (Math.PI / 2));
-    
+
+            pas.xoff = s * Math.sin((5 - a) * (Math.PI / 2));
+            pas.yoff = s * Math.sin(a * (Math.PI / 2));
+
             pas.color = shell.color;
             pas.size = Math.sqrt(shell.size);
-    
+
             if (pass.length < 1000) { pass.push(pas); }
         }
     }
@@ -70,50 +101,50 @@ function firework(){
 
         var dt = 1;
         if (lastRun != 0) { dt = Math.min(50, (performance.now() - lastRun)); }
-            lastRun = performance.now();
-    
+        lastRun = performance.now();
+
         //ctx.clearRect(0, 0, cwidth, cheight);
-            ctx.fillStyle = "rgba(0,0,0,0.25)";
-            ctx.fillRect(0, 0, cwidth, cheight);
-    
+        ctx.fillStyle = "rgba(0,0,0,0.25)";
+        ctx.fillRect(0, 0, cwidth, cheight);
+
         if ((shells.length < 10) && (Math.random() > 0.96)) { newShell(); }
-    
+
         for (let ix in shells) {
-    
+
             var shell = shells[ix];
-    
+
             ctx.beginPath();
             ctx.arc(shell.x * cwidth, shell.y * cheight, shell.size, 0, 2 * Math.PI);
             ctx.fillStyle = shell.color;
             ctx.fill();
-    
+
             shell.x -= shell.xoff;
             shell.y -= shell.yoff;
             shell.xoff -= (shell.xoff * dt * 0.001);
             shell.yoff -= ((shell.yoff + 0.2) * dt * 0.00005);
-    
+
             if (shell.yoff < -0.005) {
-            newPass(shell);
-            shells.splice(ix, 1);
+                newPass(shell);
+                shells.splice(ix, 1);
             }
         }
-    
+
         for (let ix in pass) {
-    
+
             var pas = pass[ix];
-    
+
             ctx.beginPath();
             ctx.arc(pas.x, pas.y, pas.size, 0, 2 * Math.PI);
             ctx.fillStyle = pas.color;
             ctx.fill();
-    
+
             pas.x -= pas.xoff;
             pas.y -= pas.yoff;
             pas.xoff -= (pas.xoff * dt * 0.001);
             pas.yoff -= ((pas.yoff + 5) * dt * 0.0005);
             pas.size -= (dt * 0.002 * Math.random())
-    
-            if ((pas.y > cheight)  || (pas.y < -50) || (pas.size <= 0)) {
+
+            if ((pas.y > cheight) || (pas.y < -50) || (pas.size <= 0)) {
                 pass.splice(ix, 1);
             }
         }
